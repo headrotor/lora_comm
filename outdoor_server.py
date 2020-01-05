e7f7c40f0cfc554fb2765db27c7cb315c32037d9
headrotor <rrmutt@gmail.com>
2020-01-04 21:20:38 -0800

    checkpoint

--- outdoor_server.py
+++ outdoor_server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # coding=utf-8
 """
 ----------------------------------------------------------------------
@@ -11,6 +12,7 @@ import sys
 import time 
 import traceback
 import serial
+import socket
 # local serial wrapper class
 #sys.path.append('../common')
 import simplejson
@@ -18,8 +20,48 @@ import simplejson
 
 from ubidots import ApiClient
 
+def carbon_write(varname, value):
+    sock = socket.socket()
+    sock.connect( ("localhost", 2003) )
+    msg = "{}.metric {} {}\n".format(varname, value, time.time())
+    sock.send(msg.encode('utf-8'))
+    sock.close()
+    print("sent carbon " + msg)
 
 
+def ubidots_write(varname, value):
+    var = None
+    try:
+        if varname == "temp:":
+            var = api.get_variable('5dfb1a7e1d84721740527b94')
+        elif varname == "baro:":
+            var = api.get_variable('5dfb27f91d84722f098dd73b')             
+        elif varname == "humid:":
+            var = api.get_variable('5dfbc00c1d847274748affbd')
+        elif varname == "mail:":
+            var = api.get_variable('5dfbc10c1d84727786722701')             
+        elif varname == "gasr:":
+            var = api.get_variable('5dfbc1431d847277843eb54d')             
+        elif varname == "angy:":
+            var = api.get_variable('5dfbc17a1d847278d932665e')             
+        elif varname == "shake:":
+            var = api.get_variable('5dfbc1961d84727933e5b16f')
+
+        elif varname == "door:":
+            var = api.get_variable('5dfcf6be1d8472084f0d6e7c')
+
+
+    except simplejson.scanner.JSONDecodeError:
+        return
+
+    if var is not None:
+        try:
+            val = var.save_value({'value': float(value)})
+            #print("sent val")
+        except ValueError:
+            print("No JSON received, skipping")
+
+    
 def parse_message(thestr, logfn, api):
     if len(instr) == 0:
         return
@@ -37,8 +79,8 @@ def parse_message(thestr, logfn, api):
     #print(str(parse))
     if len(parse) == 3:
         if parse[0] == 'Message:':
-            print(instr.strip())
-            print("{}, {}, {}".format(time.time(), parse[1], parse[2]))
+            #print(instr.strip())
+            print("{}, {}, {}".format(time.strftime('%l:%M%p %Z on %b %d, %Y'), parse[1], parse[2]))
             with open(logfn, "a") as logfile:
                 logfile.write("{}, {}, {}\n".format(time.time(),
                                                     ndict[parse[1]],
@@ -47,39 +89,19 @@ def parse_message(thestr, logfn, api):
             
             var = None
             try:
-                if parse[1] == "temp:":
-                    var = api.get_variable('5dfb1a7e1d84721740527b94')
-                elif parse[1] == "baro:":
-                    var = api.get_variable('5dfb27f91d84722f098dd73b')             
-                elif parse[1] == "humid:":
-                    var = api.get_variable('5dfbc00c1d847274748affbd')
-                elif parse[1] == "mail:":
-                    var = api.get_variable('5dfbc10c1d84727786722701')             
-                elif parse[1] == "gasr:":
-                    var = api.get_variable('5dfbc1431d847277843eb54d')             
-                elif parse[1] == "angy:":
-                    var = api.get_variable('5dfbc17a1d847278d932665e')             
-                elif parse[1] == "shake:":
-                    var = api.get_variable('5dfbc1961d84727933e5b16f')
-
-                elif parse[1] == "door:":
-                    var = api.get_variable('5dfcf6be1d8472084f0d6e7c')
-
-                    
-            except simplejson.scanner.JSONDecodeError:
-                return
-
-            if var is not None:
-                try:
-                    val = var.save_value({'value': float(parse[2])})
-                    print("sent val")
-                except ValueError:
-                    print("No JSON received, skipping")
-
-
+                ubidots_write(parse[1], parse[2])
+            except Exception as e:
+                print("caught ubidots exception")
+                raise e
+            try:
+                carbon_write(parse[1], parse[2])
+            except Exception as e:
+                print("caught carbon exception")
+                raise e
+            sys.stdout.flush()
             
 if __name__ == '__main__':
-    portname = '/dev/ttyUSB1'
+    portname = '/dev/ttyUSB0'
     portbaud = '115200'
     #portbaud = '120000000'
 
@@ -88,7 +110,7 @@ if __name__ == '__main__':
 
     api = ApiClient(token='BBFF-gjV5Rkr2FXKR4DRCwjDvlBRSjruPKP')
 
-
+    print("starting")
     
     try:
         ser = serial.Serial(portname,portbaud,timeout=0.1)
@@ -112,7 +134,8 @@ if __name__ == '__main__':
         if len(instr) > 0:
             #print(instr)
             parse_message(instr, logfname, api)
- 
+            sys.stdout.flush()
+            
         now = time.time()
 
         #if (now - then) > 2:
